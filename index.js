0fe871a6bdcf418f3bbcc8ea9e3526c4acf36f8a
Eva Leake <teasacuraster@gmail.com>
2018-03-06 16:45:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+  var newObj = Object.assign({}, key)
+  delete newObj.key
+  return newObj
 }
